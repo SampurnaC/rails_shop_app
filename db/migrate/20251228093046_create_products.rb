4ff39040e3b44f5d0f19b08d51ffb42625247b7a
sam <sampurna.chapagainnn@gmail.com>
2025-12-28 10:18:21 +0000

    category and product migration files

---
+++ db/migrate/20251228093046_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[8.0]
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.text :description
+      t.decimal :price
+      t.integer :status
+      t.references :user, null: false, foreign_key: true
+      t.references :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
