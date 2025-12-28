33020b4e9af706ff8e9578e38040e8744bbf6f8d
sam <sampurna.chapagainnn@gmail.com>
2025-12-28 10:33:31 +0000

    create crud for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,49 @@
 class ProductsController < ApplicationController
 
+  before_action :authenticate_user!
+  before_action :set_product, only: %i[show, edit, update, destroy]
   def index
+    @products=Product.all
+  end
+
+  def new
+    @product=current_user.products.build
+  end
+
+  def create
+    @product=current_user.products.build(product_params)
+    if @product.save
+      redirect_to @product, notice: "Product submitted for review"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+
+  def update
+    if @product.update(product_params)
+      redirect_to @product, notice: "Product successfully updated"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @product.destroy
+      redirect_to root_path, notice: "Product successfully deleted"
+    end
+  end
+
+  private
+  
+  def set_product
+    @product=Product.find(params[:id])
+  end
+  def product_params
+    params.require(:title, :description, :price, :status)
   end
 
 end
