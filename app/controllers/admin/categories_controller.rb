571babc76c8d75f3fe451fe3a45c6d2a560af48b
sam <sampurna.chapagainnn@gmail.com>
2025-12-28 23:33:23 +0000

    work on crud for admin-categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,55 @@
 class Admin::CategoriesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_admin
+  before_action :set_category, only: %i[show edit update destroy]
+  def index
+    @categories=Category.all
+  end
+
+  def show
+  end
+
+  def new
+    @category=Category.new
+  end
+
+  def create
+    @category=Category.new(category_params)
+    if @category.save
+      redirect_to admin_category_path(@category), notice: "Category successfully created"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to @category, notice: "Category successfully updated"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to admin_categories_path, notice: "Category successfully deleted"
+  end
+
+  private
+
+  def require_admin
+    redirect_to root_path, alert: "Not authorized" unless current_user&.admin?
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
 end
