2f3dc32006f9beabd6439d7f481e8baff1defad0
sam <sampurna.chapagainnn@gmail.com>
2026-01-07 08:55:14 +0000

    simplecov configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,8 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 # that will avoid rails generators crashing because migrations haven't been run yet
 # return unless Rails.env.test?
 require 'rspec/rails'
+require 'simplecov'
+SimpleCov.start
 #require 'devise'
 #require 'devise/strategies/authenticatable'
 #Devise.warden_config.default_strategies(:scope => :user)
