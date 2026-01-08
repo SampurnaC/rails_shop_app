a5ed40dfdcead53551fd83c4ba42b15e0a581e04
sam <sampurna.chapagainnn@gmail.com>
2026-01-08 12:48:48 +0000

    refactor product spec by removing category and user create

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,17 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
 
-  let(:user) {User.create(email: "test@test.com", password: "123456", password_confirmation: "123456")}
-  let(:category){Category.create(name: "dammi category")}
-
   it "is valid with valid attributes" do
-    product=Product.new(title: "dammi product", price: 100, user: user, category: category)
-    expect(product).to be_valid
+    expect(build(:product)).to be_valid
   end
 
   it "is not valid with invalid arrributes" do
-    product=Product.new(price: 100, user: user, category: category)
-    expect(product).to_not be_valid
+    expect(build(:product, title: nil)).not_to be_valid
   end
 
 end
