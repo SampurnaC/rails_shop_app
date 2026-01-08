483638ba1de5513f1c4b88ef8a5e5f61bf7c5214
sam <sampurna.chapagainnn@gmail.com>
2026-01-08 12:52:18 +0000

    add invalid category test case with passing empty name

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe Category, type: :model do
     expect(build(:category)).to be_valid
   end
 
+  it "is not valid with valid attributes" do
+    expect(build(:category, name: nil)).to_not be_valid
+  end
+
 end
