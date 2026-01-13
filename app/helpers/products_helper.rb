82a314d1e2ad50ae7d08b8b6a9fa88419521231a
sam <sampurna.chapagainnn@gmail.com>
2026-01-13 22:36:03 +0000

    product helper to check if favorite exists for specific product

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,7 @@
 module ProductsHelper
+  def favorited?(product)
+    return false unless current_user && product
+    user_favorites = current_user.favorites
+    user_favorites.exists?(product_id: product.id)
+  end
 end
