ae66048dfb10990bcf1cc65223085a44f8945f84
sam <sampurna.chapagainnn@gmail.com>
2026-03-01 00:15:31 +0000

    add order relationship with products and set enum status

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many :order_items, dependent: :destroy
+  has_many :products, through: :order_items
+
+  enum :status, {cart: 0, placed: 1, shipped: 2}
+
 end
